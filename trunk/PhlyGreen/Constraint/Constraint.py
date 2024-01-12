d96f8494fc18da221e3be4f177f6b8c932203899
rmalpica <riccardp.malpica@gmail.com>
2024-01-12 17:24:00 +0100

    fixing climb and OEI constraint, adjusted input vals for ATR42

--- trunk/PhlyGreen/Constraint/Constraint.py
+++ trunk/PhlyGreen/Constraint/Constraint.py
@@ -18,7 +18,8 @@ class Constraint:
         self.DISA = self.aircraft.ConstraintsInput['DISA']
         self.kTO = self.aircraft.ConstraintsInput['kTO']
         self.sTO = self.aircraft.ConstraintsInput['sTO']
-        self.CB = self.aircraft.ConstraintsInput['Climb Gradient']
+        self.CB = self.aircraft.ConstraintsInput['OEI Climb Gradient']
+        self.ROC = self.aircraft.ConstraintsInput['Rate of Climb']
         self.ht = self.aircraft.ConstraintsInput['ht']
         self.M1 = self.aircraft.ConstraintsInput['M1']
         self.M2 = self.aircraft.ConstraintsInput['M2']
@@ -29,16 +30,16 @@ class Constraint:
         
 
 
-    def EvaluateConstraints(self, WTOoS, DISA, kTO, sTO, CB, ht, PsAcceleration):
+    def EvaluateConstraints(self, WTOoS, DISA, kTO, sTO, CB, ROC, ht, PsAcceleration):
         
         
         
         self.PWCruise = self.aircraft.performance.PoWTO(self.WTOoS, self.ConstraintsBeta[0], 0, self.ConstraintsN[0], self.ConstraintsAltitude[0], DISA, self.ConstraintsSpeed[0], self.ConstraintsSpeedtype[0])
         self.PWTakeOff = self.aircraft.performance.TakeOff(WTOoS,self.ConstraintsBeta[1], self.ConstraintsAltitude[1], kTO, sTO, DISA, self.ConstraintsSpeed[1], self.ConstraintsSpeedtype[1])
-        self.PWOEI = self.aircraft.performance.PoWTO(self.WTOoS,self.ConstraintsBeta[2], 1.4*CB*self.ConstraintsSpeed[2], self.ConstraintsN[2], self.ConstraintsAltitude[2], DISA, self.ConstraintsSpeed[2], self.ConstraintsSpeedtype[2])
-        #self.PWOEI = self.aircraft.performance.OEIClimb(self.WTOoS,self.ConstraintsBeta[2], 1.4*CB*self.ConstraintsSpeed[2], self.ConstraintsN[2], self.ConstraintsAltitude[2], DISA, self.ConstraintsSpeed[2], self.ConstraintsSpeedtype[2])
-        #old#self.PWOEI = self.aircraft.performance.OEIClimb(self.WTOoS,self.ConstraintsBeta[2], 80*0.012, self.ConstraintsN[2], self.ConstraintsAltitude[1], DISA, 80, self.ConstraintsSpeedtype[2])
-        self.PWClimb = self.aircraft.performance.PoWTO(self.WTOoS,self.ConstraintsBeta[2], 1.4*CB*self.ConstraintsSpeed[2], self.ConstraintsN[2], self.ConstraintsAltitude[2], DISA, self.ConstraintsSpeed[2], self.ConstraintsSpeedtype[2])
+        #self.PWOEI = self.aircraft.performance.PoWTO(self.WTOoS,self.ConstraintsBeta[2], CB*self.ConstraintsSpeed[2], self.ConstraintsN[2], self.ConstraintsAltitude[2], DISA, self.ConstraintsSpeed[2], self.ConstraintsSpeedtype[2])
+        self.PWOEI = self.aircraft.performance.OEIClimb(self.WTOoS,self.ConstraintsBeta[2], 0.8*CB*self.ConstraintsSpeed[1], self.ConstraintsN[2], self.ConstraintsAltitude[2], DISA, 0.8*self.ConstraintsSpeed[2], self.ConstraintsSpeedtype[2])
+        self.PWClimb = self.aircraft.performance.PoWTO(self.WTOoS,self.ConstraintsBeta[2], ROC, self.ConstraintsN[2], self.ConstraintsAltitude[2], DISA, self.ConstraintsSpeed[2], self.ConstraintsSpeedtype[2])
+        #self.PWOEI = self.aircraft.performance.PoWTO(self.WTOoS,self.ConstraintsBeta[2], ROC, self.ConstraintsN[2], self.ConstraintsAltitude[2], DISA, self.ConstraintsSpeed[2], self.ConstraintsSpeedtype[2])
         self.PWTurn = self.aircraft.performance.PoWTO(self.WTOoS, self.ConstraintsBeta[3], 0, self.ConstraintsN[3], self.ConstraintsAltitude[3], DISA, self.ConstraintsSpeed[3], self.ConstraintsSpeedtype[3])
         self.PWCeiling = self.aircraft.performance.Ceiling(WTOoS, self.ConstraintsBeta[4], ht, self.ConstraintsN[4], self.ConstraintsAltitude[4], DISA, self.ConstraintsSpeed[4])
         self.PWAcceleration = self.aircraft.performance.PoWTO(self.WTOoS,self.ConstraintsBeta[5], PsAcceleration, self.ConstraintsN[5], self.ConstraintsAltitude[5], DISA, self.Mavg, self.ConstraintsSpeedtype[5])
@@ -50,7 +51,7 @@ class Constraint:
     def FindDesignPoint(self):
 
            
-        self.EvaluateConstraints(self.WTOoS, self.DISA, self.kTO, self.sTO, self.CB, self.ht, self.PsAcceleration)
+        self.EvaluateConstraints(self.WTOoS, self.DISA, self.kTO, self.sTO, self.CB, self.ROC, self.ht, self.PsAcceleration)
         
         PWMatrix = np.matrix([self.PWCruise, self.PWTakeOff, self.PWClimb, self.PWTurn, self.PWCeiling, self.PWAcceleration, self.PWOEI])
         self.MaxPW = np.zeros(len(self.WTOoS))
