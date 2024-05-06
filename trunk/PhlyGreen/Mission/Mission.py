e9e82bb6160d92cf365e2c0a24eee6c807a95c70
João Delille <joao.delille@tecnico.ulisboa.pt>
2024-05-06 19:27:01 +0200

    WIP added SOC functions, doesnt work tho

--- trunk/PhlyGreen/Mission/Mission.py
+++ trunk/PhlyGreen/Mission/Mission.py
@@ -57,7 +57,7 @@ class Mission:
 
         self.beta0 = self.aircraft.MissionInput['Beta start']
         self.ef = self.aircraft.EnergyInput['Ef']
-
+        """TODO remember to add here an input for SOC_0"""
     def InitializeProfile(self):
         
         self.profile = Profile(self.aircraft)
@@ -160,36 +160,37 @@ class Mission:
 
         
         def model(t,y):
-            
-            Beta = y[2] ##this is the aircraft mass fraction
-            SOC = y[3]
+
+            Beta = y[2] #aircraft mass fraction
+            SOC = y[3]  #battery state of charge
             Ppropulsive = PowerPropulsive(Beta,t)
             PRatio = self.aircraft.powertrain.Hybrid(self.aircraft.mission.profile.SuppliedPowerRatio(t),self.profile.Altitude(t),self.profile.Velocity(t),Ppropulsive) #takes in all the mission segments and finds the required power ratio for the current time of the mission
-            
+
             dEFdt = Ppropulsive * PRatio[0] #fuel power output
             dbetadt = - dEFdt/(self.ef*self.WTO) #change in mass due to fuel consumption
 
             PElectric = Ppropulsive * PRatio[5] #propulsive power required for the electric motors
             BatVolt = self.aircraft.battery.SOC_2_OC_Voltage(SOC) #open circuit voltage of the battery at the current SOC
-            BatCurr = self.aircraft.battery.Power_2_Current(SOC, PElectric) #current output of the battery at the current motor power draw
-            
+            BatCurr = self.aircraft.battery.Power_2_Current(SOC, PElectric, self.aircraft.battery.parallel_stack_number) #current output of the battery at the current motor power draw
+
             dEBatdt = BatVolt * BatCurr #actual power drawn from the battery, losses included
             dSOCdt = 1-dEBatdt/self.aircraft.battery.pack_energy #gives the rate of change of SOC over time
-            
+
             return [dEFdt,dEBatdt,dbetadt,dSOCdt]
 
         # Takeoff condition
-        Ppropulsive = self.WTO * self.aircraft.performance.TakeOff(self.aircraft.DesignWTOoS,self.aircraft.constraint.TakeOffConstraints['Beta'], self.aircraft.constraint.TakeOffConstraints['Altitude'], self.aircraft.constraint.TakeOffConstraints['kTO'], self.aircraft.constraint.TakeOffConstraints['sTO'], self.aircraft.constraint.DISA, self.aircraft.constraint.TakeOffConstraints['Speed'], self.aircraft.constraint.TakeOffConstraints['Speed Type']) ##calculates the propulsive power required for takeoff
+        Ppropulsive = self.WTO * self.aircraft.performance.TakeOff(self.aircraft.DesignWTOoS,self.aircraft.constraint.TakeOffConstraints['Beta'], self.aircraft.constraint.TakeOffConstraints['Altitude'], self.aircraft.constraint.TakeOffConstraints['kTO'], self.aircraft.constraint.TakeOffConstraints['sTO'], self.aircraft.constraint.DISA, self.aircraft.constraint.TakeOffConstraints['Speed'], self.aircraft.constraint.TakeOffConstraints['Speed Type']) #calculates the propulsive power required for takeoff
 
-        PRatio = self.aircraft.powertrain.Hybrid(self.aircraft.mission.profile.SPW[0][0],self.aircraft.constraint.TakeOffConstraints['Altitude'],self.aircraft.constraint.TakeOffConstraints['Speed'],Ppropulsive) ##power ratio for takeoff
-        self.TO_PBat = Ppropulsive * PRatio[5] ##battery power during takeoff, again, wrong because it lacks the effect of internal resistance
-        self.TO_PP = Ppropulsive * PRatio[1]  
+        PRatio = self.aircraft.powertrain.Hybrid(self.aircraft.mission.profile.SPW[0][0],self.aircraft.constraint.TakeOffConstraints['Altitude'],self.aircraft.constraint.TakeOffConstraints['Speed'],Ppropulsive) #hybrid power ratio for takeoff
+        self.TO_PP = Ppropulsive * PRatio[1] #combustion engine power during takeoff
+        self.TO_PBat = Ppropulsive * PRatio[5] #electric motor power during takeoff
+        self.TO_PBat_Cells = self.aircraft.battery.Power_2_Parallel_Cells(1,self.TO_PBat) #1 here is the SOC at takeoff, assumes fully charged batteries TODO CONSIDER CHANGING LATER
 
         #set/reset max values
         self.Max_PBat = -1
         self.Max_PEng = -1
 
-        y0 = [0,0,self.beta0] ##integration starting point for the spent battery and fuel energy as well as mass fraction
+        y0 = [0,0,self.beta0,1] ##integration starting point for the spent battery and fuel energy as well as mass fraction and SOC
 
         rtol = 1e-5
         method= 'BDF'
@@ -210,34 +211,39 @@ class Mission:
 
         self.Ef = sol.y[0]
         self.EBat = sol.y[1] 
-        """
-        have this stuff be converted to number of cells using the battery functions, return it at the end?
-        """
         self.Beta = sol.y[2]
+
 
         # compute peak Propulsive power along mission
         times = []
         beta = []
+        SOC = []
         for array in self.integral_solution:
             times = np.concatenate([times, array.t])
             beta = np.concatenate([beta, array.y[2]])
+            SOC = np.concatenate([SOC, array.y[3]])
 
-        PP = np.array([WTO * self.aircraft.performance.PoWTO(self.aircraft.DesignWTOoS,beta[i],self.profile.PowerExcess(times[i]),1,self.profile.Altitude(times[i]),self.DISA,self.profile.Velocity(times[i]),'TAS') for i in range(len(times))])
-        PRatio = np.array([self.aircraft.powertrain.Hybrid(self.aircraft.mission.profile.SuppliedPowerRatio(times[i]),self.profile.Altitude(times[i]),self.profile.Velocity(times[i]),PP[i]) for i in range(len(times))] )
+        PP = np.array([WTO * self.aircraft.performance.PoWTO(self.aircraft.DesignWTOoS,beta[i], self.profile.PowerExcess(times[i]), 1, self.profile.Altitude(times[i]), self.DISA,self.profile.Velocity(times[i]), 'TAS') for i in range(len(times))])
+
+        PRatio = np.array([self.aircraft.powertrain.Hybrid(self.aircraft.mission.profile.SuppliedPowerRatio(times[i]), self.profile.Altitude(times[i]), self.profile.Velocity(times[i]), PP[i]) for i in range(len(times))] )
+
         self.Max_PEng = np.max(np.multiply(PP,PRatio[:,1]))
-        self.Max_PBat = np.max(np.multiply(PP,PRatio[:,5])) 
-        """
-        #need to find here what to do so that this uses both the peak power AND the SOC at peak power
-        to compute the nr of cells using self.aircraft.battery.Power_2_Parallel_Cells(SOC, self.Max_PBat) and thats what gets returned back to the battery configurator function
-        """
+        #self.Max_PBat = np.max(np.multiply(PP,PRatio[:,5])) # <- replace with max nr of cells required for power
+        self.PwrBatArray = np.multiply(PP,PRatio[:,5])
 
+        self.CellBatArray = []
+        #get all the equivalent parallel cells needed for power at each point of the flight and put them all in an array
+        for i in range(len(self.PBatArray)):
+            instantCells=self.aircraft.battery.Power_2_Parallel_Cells(SOC[i], self.PwrBatArray[i]) 
+            self.CellBatArray = np.concatenate([self.CellBatArray, instantCells])
+        self.Max_PBat_Cells = np.max(self.CellBatArray)
+
+        #returns the required Fuel Energy & Battery Energy
         return self.Ef[-1], self.EBat[-1]
-        
-        
+
+
 """
 ok to do: 
-line 200
-line 214
-line 230
-then figure out how to initialize things in the battery.py file
+line 60
+then figure out how to initialize things in the battery.py file - done?
 """
--- trunk/PhlyGreen/Systems/Battery/Battery.py
+++ trunk/PhlyGreen/Systems/Battery/Battery.py
@@ -13,6 +13,7 @@ class Battery:
         self.required_energy = None
         self.required_power = None
         self.parallel_stack_number = 1 #initial value so things can be calculated at all, does this work?
+        self.pack_energy = None
         self.controller_Vmax = 740 
         self.controller_Vmin = 420 #this range of voltages should be defined in the model of the motor controller, but ill do that later, for now its hardcoded
 
@@ -35,13 +36,14 @@ class Battery:
         #self.series_stack_size = np.max([np.floor(self.controller_Vmax/self.cell_Vmax), np.ceil(self.controller_Vmin/self.cell_Vmin) ])
         # ^^ replaced by version that only considers vmax, i believe that to be correct but i need to investigate to be sure
         self.series_stack_size = np.floor(self.controller_Vmax/self.cell_Vmax) #number of cells in series to achieve desired voltage
-        
+        self.pack_energy = self.parallel_stack_number*self.series_stack_size*self.cell_energy #initializing this at a non zero value
 
 #determine battery configuration
     #def Configuration(self,required_energy, required_power): # cant work like this anymore, cells for power are calculated externally
+    #must receive the number of cells in parallel for energy and power
     def Configuration(self, parallel_cells_energy, parallel_cells_power):
 
-        
+        '''
         #number of cells in series per stack, imposed by whatever motor controller we are using, as those have a fixed operational voltage range that cant be exceeded
         #is this the best way of doing it? or should the optimizer be able to tweak this? - find if there is an optimal way of choosing
         
@@ -51,7 +53,8 @@ class Battery:
         #self.parallel_cells_energy = np.ceil(total_cells_energy/self.series_stack_size)
 
         #cant work like this anymore, cells are calculated outside of here
-        #self.parallel_nr_power = np.ceil(required_power/(self.controller_Vmax*self.cell_current)) 
+        #self.parallel_nr_power = np.ceil(required_power/(self.controller_Vmax*self.cell_current))
+        '''
 
         #number of cell stacks in parallel required while tracking which constraint is the driver
         if parallel_cells_energy > parallel_cells_power:
@@ -79,9 +82,9 @@ class Battery:
 
     #calculate the SOC from the energy spent so far
     def Energy_2_SOC(self, E):
-        Q_0 = self.pack_energy
-        Q   = Q_0 - E
-        SOC = Q / Q_0
+        Q_0 = self.pack_energy #total charge
+        Q   = Q_0 - E          #remaining charge
+        SOC = Q / Q_0          #SOC gives remaining charge %
         return SOC
 
     #convert SOC to open circuit voltage using the linear relation. possibly expand to include the exponential zones?
@@ -91,17 +94,36 @@ class Battery:
         return Pack_U_oc
 
     #Calculates the open circuit voltage and current to enable calculating real power drain from the battery in function of useful output power. U_oc is the open circuit voltage, U_out is the measured battery output voltage
-    def Power_2_Current(self, SOC, Power_out):
-        Resistance = self.cell_resistance * self.series_stack_size
+    def Power_2_Current(self, SOC, Power_out, nr_parallel_cells):
+        Resistance = self.cell_resistance * self.series_stack_size / nr_parallel_cells #calculate total internal R of the pack
         U_oc = self.SOC_2_OC_Voltage(SOC)
-        U_out = (U_oc + (U_oc**2 - 4 * Power_out * Resistance)**0.5)/2 #this comes from the solution of P_out = U_out * I_out
+        U_out = (U_oc + (U_oc**2 - 4 * Power_out * Resistance)**0.5)/2 #this comes from the analytical solution of P_out = U_out * I_out
         I_out = (U_oc - U_out)/Resistance
         return I_out
 
     #find the number of cells required to supply the requested current at the current SOC. Assumes that the motor controller will draw a higher current to compensate for the lower input voltage as SOC goes down. Power is useful power output to the motor controller.
     def Power_2_Parallel_Cells(self, SOC, Power_out):
-        required_current = self.Power_2_Current(SOC, Power_out)
-        parallel_cells = np.ceil(required_current/self.cell_current)
+        #iterates a couple times until the nr of cells doesnt change anymore since the current is affected by the nr of cells in parallel, and vice versa
+        #should this be replaced with a brent function to ensure convergence?
+        """U_oc = self.SOC_2_OC_Voltage(SOC)
+        p_cells_0=
+        parallel_cells = np.ceil(self.cell_resistance * self.series_stack_size*(4*Power_out)/U_oc**2)
+        while (p_cells_0!=parallel_cells):
+            p_cells_0=parallel_cells
+            Resistance = self.cell_resistance * self.series_stack_size / p_cells_0 #calculate total internal R of the pack
+            U_out = (U_oc + (U_oc**2 - 4 * Power_out * Resistance)**0.5)/2 #this comes from the analytical solution of P_out = U_out * I_out
+            I_out = (U_oc - U_out)/Resistance
+            parallel_cells = np.ceil(I_out/self.cell_current)
+            parallel_cells = np.ceil(self.cell_resistance * self.series_stack_size*(4*Power_out)/U_oc**2)
+            replace this with brent function????? idk
+            """
+        U_oc = self.SOC_2_OC_Voltage(SOC)
+        parallel_cells = np.ceil(self.cell_resistance * self.series_stack_size*(4*Power_out)/U_oc**2)
+        Resistance = self.cell_resistance * self.series_stack_size / parallel_cells
+        U_out = (U_oc + (U_oc**2 - 4 * Power_out * Resistance)**0.5)/2
+        I_out = (U_oc - U_out)/Resistance
+        parallel_cells = np.ceil(I_out/self.cell_current)
+        print(parallel_cells)
         return parallel_cells
 
     #find the number of cells in parallel required to obtain the total energy necessary assuming the number of cells in series is known
@@ -110,3 +132,7 @@ class Battery:
         parallel_cells = np.ceil(total_cells/self.series_stack_size)
         return parallel_cells
 
+""" TODO_LIST:
+consider changing the functions in this file to use arrays instead of single constant values? 
+in Power_2_Parallel_Cells() FIX IT because its badly broken right now
+"""
