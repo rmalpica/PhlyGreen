f18745dc34a712ce406b4403ebea75eea1814319
Joao Delille <joao.delille@tecnico.ulisboa.pt>
2024-10-07 17:02:55 +0100

    update testing script

--- trunk/Validation/validate1.py
+++ trunk/Validation/validate1.py
@@ -2,25 +2,22 @@
 import FlightSweeps
 import PlotJSONs
 import os
-# This will configure run and plot everything relating to a single sweep
-def runAndPlotAll(aL,runName,varsOfInterest):
-    if len(varsOfInterest['In']) != 2:
-        raise ValueError("Must provide two values to plot against")
 
-    #first configure the output directories
+# This will configure run and plot everything relating to a single sweep
+# receives:
+# - argument list
+# - name under which the data should be saved
+# - optional variables to be plotted in extra plots
+def runAndPlot(aL,runName,varsOfInterest={}):
+
+    # First configure the output directories
     FlightSweeps.Configure(runName)
     dirJSONs = FlightSweeps.json_directory
     dirPlots = os.path.join(FlightSweeps.dirOutput,"Plots")
-    dirExtraPlots= os.path.join(FlightSweeps.dirOutput,'Extras','Plots')
-    jsonExtras=os.path.join(FlightSweeps.dirOutput,'Extras','designspace.json')
     os.makedirs(dirPlots,exist_ok=True)
-    os.makedirs(dirExtraPlots,exist_ok=True)
 
     # FlightSweeps.main runs everything needed to sweep the parameters given
-    # run multiple times with different lists to sweep different things
     # it runs a nested loop of all the different combinations given
-    # its preferrable to sweep the Phi Range and Payload one at a time, to avoid wasting time
-    # on hundreds of pointless combinations
     # in Traditional configuration, the cells and the phis are ignored
     FlightSweeps.main(aL['ArchList'],aL['MissionList'],aL['RangesList'],aL['PayloadsList'],aL['CellsList'],aL['PhisList'])
 
@@ -28,38 +25,61 @@ def runAndPlotAll(aL,runName,varsOfInterest):
     print('----------------------\nPlotting')
     PlotJSONs.plotFlights(dirJSONs,dirPlots)
 
-    # Then collect the JSON files and make a json of the design space
-    # which contains the inputs and outputs of every design so that
-    # plots of inputs vs outputs can be made for the sweeps to compare
-    # different configurations
-    print('----------------------\nExtras')
-    PlotJSONs.scanDesigns(dirJSONs,jsonExtras)
-    PlotJSONs.extraPlots(jsonExtras,varsOfInterest,dirExtraPlots)
+    # begin the extra multi variable plots if said variables are passed as input
+    if varsOfInterest!={}:
+        print('----------------------\nExtras')
 
-# This will configure run and plot just the individual flights of a sweep.
-# Useful for running multiple different missions to validate
-#def runAndPlotMost(aL,runName):
-#    FlightSweeps.Configure('Logs',runName)
-#    FlightSweeps.main(aL['ArchList'],aL['MissionList'],aL['RangesList'],aL['PayloadsList'],aL['CellsList'],aL['PhisList'])
-#    PlotJSONs.plotFlights(dirJSONs,dirPlots)
-#    PlotJSONs.scanDesigns(FlightSweeps.json_directory,runName+"-designspace.json")
+        if len(varsOfInterest['In']) != 2:
+            raise ValueError("Must provide two values to plot against")
 
+        # create the directories used
+        dirExtraPlots= os.path.join(FlightSweeps.dirOutput,'Extras','Plots')
+        jsonExtras=os.path.join(FlightSweeps.dirOutput,'Extras','designspace.json')
+        os.makedirs(dirExtraPlots,exist_ok=True)
+        
+        # scan the json files to create a list of the flights and their parameters
+        PlotJSONs.scanDesigns(dirJSONs,jsonExtras)
+
+        # use that list to plot what was requested
+        PlotJSONs.extraPlots(jsonExtras,varsOfInterest,dirExtraPlots)
 
 #=========================================================================================#
-# write script here using the functions above
+# write script here using the function above
 
 # define the list of parameters to sweep over
+# its best to only sweep one of the numerical values at a time to avoid having hundreds
+# of pointless mission combinations at once that are unmanageable
+
 aL={'ArchList'     :{'Hybrid','Traditional'},
     'MissionList'  :{'FelixFinger'},
-    'CellsList'    :{'FELIX_FINGER'},
-    'RangesList'   :{600,1200,1800,2400},
-    'PayloadsList' :{1500},
-    'PhisList'     :{0.1,0.2,0.3,0.4,0.5}}
-#runAndPlotMost(aL,'test')
+    'CellsList'    :{'SAMSUNG_LIR18650','FELIX_FINGER'},
+    'RangesList'   :{600,2100},
+    'PayloadsList' :{1200},
+    'PhisList'     :{0.1,0.3,0.5}}
+
 # define which values from the output json should be multiplotted,
 # and which should be considered the TWO inputs that were swept
 # the names here are passed to the plotter so they have to be the keys found on the output json
-varsOfInterest={'In':['Range','Base Phi'],
+varsOfInterest={'In':['Range','Base Phi'], # lets see how range affects the design at different phi values
+                'To Plot':['Structure Mass',
+                           'Wing Surface',
+                           'Battery Mass',
+                           'Takeoff Weight',
+                           'Climb Cruise Battery PP'
+                          ]}
+# specify the list and name to use, specify a non empty varsOfInterest in order to run the extra plots
+runAndPlot(aL,'Sweep1',varsOfInterest=varsOfInterest)
+
+
+#################
+aL={'ArchList'     :{'Hybrid','Traditional'},
+    'MissionList'  :{'FelixFinger'},
+    'CellsList'    :{'SAMSUNG_LIR18650','FELIX_FINGER'},
+    'RangesList'   :{1800},
+    'PayloadsList' :{1200,2400},
+    'PhisList'     :{0.1,0.3,0.5}}
+
+varsOfInterest={'In':['Payload','Base Phi'], # now do different payloads instead
                 'To Plot':['Structure Mass',
                            'Wing Surface',
                            'Battery Mass',
@@ -67,6 +87,15 @@ varsOfInterest={'In':['Range','Base Phi'],
                            'Climb Cruise Battery PP'
                           ]}
 
-runAndPlotAll(aL,'TheTest',varsOfInterest)
+runAndPlot(aL,'Sweep2',varsOfInterest=varsOfInterest)
 
 
+#################
+aL={'ArchList'     :{'Hybrid','Traditional'},
+    'MissionList'  :{'FelixFinger'},
+    'CellsList'    :{'SAMSUNG_LIR18650','FELIX_FINGER'},
+    'RangesList'   :{600,2100},
+    'PayloadsList' :{1200,2400},
+    'PhisList'     :{0.1,0.5}}
+
+runAndPlot(aL,'Sweep3')
