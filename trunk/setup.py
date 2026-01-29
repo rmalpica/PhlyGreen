5e5f082f9312072c48ba34ce1619106a98a6cbed
rmalpica <riccardp.malpica@gmail.com>
2026-01-29 11:53:50 +0100

    fixing setup.py to find internal packages automatically

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
-    name="PhlyGreen", 
+setup(
+    name="PhlyGreen",
     version="0.0.1",
     author="Riccardo Malpica Galassi",
     author_email="riccardo.malpicagalassi@uniroma1.it",
     description="A collection of tools for the preliminary design of novel aircraft concepts for a more sustainable air mobility",
     url="https://github.com/rmalpica/PhlyGreen",
-    packages=['PhlyGreen'],
+    packages=find_packages(),            
+    include_package_data=True,
+    python_requires=">=3",
+    install_requires=["numpy", "matplotlib", "scipy"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3',
-    install_requires=[
-          "numpy","matplotlib","scipy"
-      ],
 )
